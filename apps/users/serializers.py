7e53ec1565e0e69848717a63ba73477ec9d1cfe9
zhanxiangyu <zhanxiangyu>
2019-04-20 14:35:59 +0800

    Base64ImageField 上传图片处理，支持base64编码

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth import get_user_model
-
-from rest_framework import serializers
-
-from .models import RecordIP
 from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from common_framework.handleserializers import Base64ImageField
+
+from users.models import RecordIP
 
 User = get_user_model()
 
 
 class UserSerializers(serializers.ModelSerializer):
-
-
     class Meta:
         model = User
         fields = "__all__"
+
     pass
 
+
 class UserPatchSerializers(serializers.ModelSerializer):
+    image = Base64ImageField(max_length=None, use_url=True)
 
     def to_internal_value(self, data):
         password = data.get('password', None)
@@ -40,6 +41,7 @@ class UserPatchSerializers(serializers.ModelSerializer):
 class RecordIPSerializers(serializers.ModelSerializer):
     user_image = serializers.SerializerMethodField()
     username = serializers.SerializerMethodField()
+
     # user_image_url = serializers.URLField(source='user.image.url')
 
     def get_user_image(self, obj):

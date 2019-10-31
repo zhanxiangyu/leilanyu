44f3d096bc3fbc1fe1f46de90e7fa5c7106ad71c
zhanxiangyu <zhanxy@cyberpeace.cn>
2019-10-31 20:41:45 +0800

    移动部分脚本文件

--- config/install_all_once.sh
+++ config/install_all_once.sh
@@ -270,9 +270,9 @@ supervisorctl update
 supervisorctl reload
 
 # 将flash_webhook.py 复制到home目录下面并 改名为webhook.py
-cp /home/leilanyu/flash_webhook.py /home/
+cp /home/leilanyu/config/flash_webhook.py /home/
 mv /home/flash_webhook.py /home/webhook.py
-# 服务器内存至少需要2个G， 不然特别卡
+# 服务器内存至少需要2个G， 不然特别卡， 还需要修改里面的Github签名签名
 # nohup python /home/webhook.py hook.log 2>&1 &
 
 
